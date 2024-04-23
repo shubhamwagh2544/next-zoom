02edf0fc5d82c8f422db8544c549b7ebc0a76bf9
shubham.wagh <shubham.wagh@globant.com>
2024-04-23 13:36:13 +0530

    new instant meeting setup ui

--- app/(root)/meeting/[id]/page.tsx
+++ app/(root)/meeting/[id]/page.tsx
@@ -1,11 +1,38 @@
-export default function ({ params }: {
+"use client"
+
+import Loader from "@/components/Loader";
+import MeetingRoom from "@/components/MeetingRoom";
+import MeetingSetup from "@/components/MeetingSetup";
+import useGetCallById from "@/hooks/useGetCallById";
+import { useUser } from "@clerk/nextjs"
+import { StreamCall, StreamTheme } from "@stream-io/video-react-sdk";
+import { useState } from "react";
+
+export default function Meeting({ params: { id } }: {
     params: {
         id: string
     }
 }) {
+
+    const { user, isLoaded } = useUser();
+    const [isSetUpComplete, setIsSetUpComplete] = useState(false);
+    const { call, isCallLoading } = useGetCallById(id);
+
+    if (!isLoaded || isCallLoading) return <Loader />
+
     return (
-        <div>
-            Meeting Room : {params.id}
-        </div>
+        <main className="h-screen w-full">
+            <StreamCall call={call}>
+                <StreamTheme>
+                    {
+                        !isSetUpComplete ? (
+                            <MeetingSetup setIsSetUpComplete={setIsSetUpComplete} />
+                        ) : (
+                            <MeetingRoom />
+                        )
+                    }
+                </StreamTheme>
+            </StreamCall>
+        </main>
     )
 }
---
+++ components/MeetingRoom.tsx
@@ -0,0 +1,7 @@
+export default function MeetingRoom() {
+    return (
+        <section className="">
+
+        </section>
+    );
+}
